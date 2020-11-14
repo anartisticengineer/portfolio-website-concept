069d7092a193ea8c135293f8abd526197f5bf0ac
anartisticengineer <justinanthonyjohnson1995@gmail.com>
2020-11-14 11:19:27 -0700

    finished navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,43 +1,56 @@
 import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import logo from "../assets/images/cyberart_by_justin_name_1.png";
+import { useEffect } from "react";
 
 const Navbar = () => {
   //toggle the dropdown menu on a mobile display
   const dropDown = () => {
-    let x = document.getElementById("theNavBar");
-    if (x.className === "nav-links") {
-      x.className += " responsive";
+    console.log('toggle');
+    let x = document.getElementById("navbar-links");
+    if (x.className === "navbar__links--hidden") {
+      x.className = "navbar__links"; //if it's hidden, open it up!
     } else {
-      x.className = "nav-links";
+      x.className = "navbar__links--hidden"; //else hide them
     }
   };
+
+  useEffect(() => {
+    document.getElementById('hamburger-btn').addEventListener('click',dropDown);
+  });
+
   const navStyle = { color: "white" };
 
   return (
     <nav className="navbar">
-      <Link to="/">
-        <img src={logo} className="navbar__logo" alt="cyberart_by_justin" id="nav-logo" />
-      </Link>
-      {/*These shoudl be hidden and collapsible in mobile view */}
-      <div className="navbar__links" id="theNavBar">
+      {/*Visible in the mobile view */}
+      <section className="navbar__header">
+        <span>
+          <Link to="/">
+            <img src={logo} className="navbar__logo" alt="cyberart_by_justin" id="nav-logo" />
+          </Link>
+        </span>
+        {/*Toggle hamburger button */}
+        <span className="navbar__hamburger-btn">
+          <a
+              href="javascript:void(0);"
+              id="hamburger-btn"
+              style={navStyle}
+            >
+            <FontAwesomeIcon icon={faBars} />
+          </a>
+        </span>
+      </section>
+      {/*These should be hidden and collapsible in mobile view */}
+      <span className="navbar__links--hidden" id="navbar-links">
         {/*Gallery dropdown */}
         <Link to="/gallery">Gallery</Link>
-        {/*End of dropdown*/}
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
-        <a
-          href="javascript:void(0);"
-          onClick={dropDown}
-          className="icon"
-          id="dropdown-icon"
-          style={navStyle}
-        >
-          <FontAwesomeIcon icon={faBars} />
-        </a>
-      </div>
+        {/*End of dropdown*/}
+      </span>
     </nav>
   );
 };

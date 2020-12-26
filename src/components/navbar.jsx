c0c240fcd92c684124cd6b5a8e9026f9dccfaf6a
anartisticengineer <justinanthonyjohnson1995@gmail.com>
2020-12-25 19:24:46 -0700

    a bit of navbar refactoring

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink} from "react-router-dom";
+import { NavLink} from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import logo from "../assets/images/cyberart_by_justin_name_1.png";
@@ -14,11 +14,7 @@ const Navbar = () => {
     <nav className="navbar">
       {/*Visible in the mobile view */}
       <section className="navbar__header">
-        <span>
-          <Link exact to="/">
-            <img src={logo} className="navbar__logo" alt="cyberart_by_justin" id="nav-logo" />
-          </Link>
-        </span>
+        <img src={logo} className="navbar__logo" alt="cyberart_by_justin" id="nav-logo"/>
         {/*Toggle hamburger button */}
         <span className="navbar__hamburger-btn" onClick={() => toggleHide(!hidden)}>
             <FontAwesomeIcon icon={hidden?faBars:faTimes} />
@@ -27,6 +23,7 @@ const Navbar = () => {
       {/*These should be hidden and collapsible in mobile view */}
       <span className={`navbar__links${hidden?'--hidden':''}`} id="navbar-links">
         {/*Gallery dropdown */}
+        <NavLink exact to="/">Home</NavLink>
         <NavLink to="/gallery">Gallery</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/contact">Contact</NavLink>
